51343c3c225f5133fb69c2d2de72fa4c3b548a22
Jonathan-W-K <162117006+Jonathan-W-K@users.noreply.github.com>
2024-03-11 13:47:02 +0300

    Update Uno.py

---
+++ pythonProject hajime/Uno.py
@@ -0,0 +1,28 @@
+# this is a sigle line comment
+"""
+this is a multiline comment
+"""
+# variables
+Name = "Shikigami"
+Age = "18"  # data type is integer
+Mark_awarded = "98.0"  # data type is float
+is_male = "true"  # data type is boolean
+
+# outputting the values in the variables
+print(Name)
+print(Age)
+print(Mark_awarded)
+
+# variable naming in python
+book_price = float(input("Enter book price: 50.00"))
+# noinspection PyRedeclaration
+book_price = "50.00"
+
+"""
+when naming the variables you cannot
+ start with numbers or anything else
+  apart from letters and '_' (Underscore) 
+"""
+
+x = z = y = 50  # assigning multiple variables the same value
+x, y, z =30, 40, 70 # assigning variables different values
